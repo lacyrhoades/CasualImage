745e7b0c2478783ee3dc8426290b71c1c9ff0bd9
Lacy Rhoades <lacyrhoades@gmail.com>
2019-02-28 16:39:05 -0500

    Made experimental color gray

--- Source/UIColor+Casual.swift
+++ Source/UIColor+Casual.swift
@@ -6,7 +6,7 @@ extension UIColor {
     }
     
     public static var experimentalColor: UIColor {
-        return UIColor(red: 255.0/255.0, green: 69.0/255.0, blue: 108.0/255.0, alpha: 1)
+        return UIColor(red: 69.0/255.0, green: 69.0/255.0, blue: 69.0/255.0, alpha: 1)
     }
 
     public static var customGray: UIColor {
